5363af8b0e695ad9ae6959621eaff77f9944bdc7
246_135 <My_email_adress@gmail.com>
2017-06-05 21:00:55 -0700

    Changed Anagram5

--- src/main/java/core/AnagramCPD.java
+++ src/main/java/core/AnagramCPD.java
@@ -42,19 +42,19 @@ public class AnagramCPD {
 	    System.out.println("Охренеть and Не хрена себе is anagram? - " + AnagramCPD.validate("Охренеть", "Не хрена себе"));
 	       }
 	
-	public void foo1()  {
-		
-		for (int i = 0; i < 10; i++) {
-				
-			for (int k = 0; k < 20; i++) {
-					
-					System.out.println("Hello");
-					
-				}
-			}
-		}
-		
-		void bar() {
-			for (;true;);
-		}
+//	public void foo1()  {
+//		
+//		for (int i = 0; i < 10; i++) {
+//				
+//			for (int k = 0; k < 20; i++) {
+//					
+//					System.out.println("Hello");
+//					
+//				}
+//			}
+//		}
+//		
+//		void bar() {
+//			for (;true;);
+//		}
 }
--- src/main/java/core/AnagramPMD.java
+++ src/main/java/core/AnagramPMD.java
@@ -25,20 +25,20 @@ public class AnagramPMD {
   }
 
 	
-	public void foo()  {
-		
-		for (int i = 0; i < 10; i++) {
-			
-			for (int k = 0; k < 20; i++) {
-				
-				System.out.println("Hello");
-			}
-		}
-	}
-	
-	void bar() {
-		for (;true;);
-	}
+//	public void foo()  {
+//		
+//		for (int i = 0; i < 10; i++) {
+//			
+//			for (int k = 0; k < 20; i++) {
+//				
+//				System.out.println("Hello");
+//			}
+//		}
+//	}
+//	
+//	void bar() {
+//		for (;true;);
+//	}
 	
 	public static void main(String[] args) {
 
